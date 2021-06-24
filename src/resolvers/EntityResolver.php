aba8f9b2bf0174289bc92a929185bdac3d297fc8
Charlie Hadden <charlie@builtbybuffalo.com>
2021-06-24 10:07:56 +0100

    Allow resolving multiple entities at once

--- src/resolvers/EntityResolver.php
+++ src/resolvers/EntityResolver.php
@@ -17,16 +17,33 @@ class EntityResolver
 {
     public static function resolve($source, array $arguments, $context, ResolveInfo $resolveInfo)
     {
+        $requestedIds = array_column($arguments['representations'], 'id');
+        $searchIds = array_unique(array_filter($requestedIds));
+
+        $entries = EntryResolver::resolve(
+            null,
+            ['id' => $searchIds],
+            $context,
+            $resolveInfo
+        );
+
+        $entriesById = [];
+
+        foreach ($entries as $entry) {
+            if (!empty($entry->id)) {
+                $entriesById[$entry->id] = $entry;
+            }
+        }
+
         return array_map(
-            function($representation) use ($context, $resolveInfo) {
-                return EntryResolver::resolveOne(
-                    null,
-                    ['id' => $representation['id']],
-                    $context,
-                    $resolveInfo
-                );
+            function($requestedId) use ($entriesById) {
+                if (empty($requestedId) || !array_key_exists($requestedId, $entriesById)) {
+                    return null;
+                }
+
+                return $entriesById[$requestedId];
             },
-            $arguments['representations']
+            $requestedIds
         );
     }
 }
